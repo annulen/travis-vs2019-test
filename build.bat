b20dbea29fed8324b24c487e5d4eb482bdd83177
Konstantin Tokarev <annulen@yandex.ru>
2020-10-06 17:26:53 +0300

    Run "conan create"

--- build.bat
+++ build.bat
@@ -1,11 +1,2 @@
 cd vorb_test\all
-python -V
-dir "C:\Program Files (x86)\Microsoft Visual Studio"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
-echo hello world
-python build-libvorbis-conan.py --configure --profile default
-python build-libvorbis-conan.py --configure --profile VS2017
+conan create . 1.3.7@dev/test --profile VS2017
