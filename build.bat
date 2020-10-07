7e87bbf3d5d3df7d935591f0fad28b603c8501c7
Konstantin Tokarev <annulen@yandex.ru>
2020-10-07 13:38:00 +0300

    Don't set CC and CXX in batch file

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 cd vorb_test\all
 python -V
-set CC=cl
-set CXX=cl
+:: set CC=cl
+:: set CXX=cl
 :: conan install . --profile=VS2017
 :: conan build --configure .
 python build-libvorbis-conan.py --configure --profile VS2017
