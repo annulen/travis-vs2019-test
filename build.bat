4c9da7d837b0a918afd989940f527eeae716155f
Konstantin Tokarev <annulen@yandex.ru>
2020-10-07 13:27:44 +0300

    With script

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@ cd vorb_test\all
 python -V
 set CC=cl
 set CXX=cl
-conan install . --profile=VS2017
-conan build --configure .
+:: conan install . --profile=VS2017
+:: conan build --configure .
+python build-libvorbis-conan.py --configure --profile VS2017
